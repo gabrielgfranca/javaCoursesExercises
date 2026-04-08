a508e1067711dd137543edf423447f9b3a047a57
Gabriel França <gabrielgfranca7@proton.me>
2026-04-08 20:21:44 -0300

    First Commit

---
+++ src/main/java/com/javacourseexercises/Main.java
@@ -0,0 +1,10 @@
+package com.javacourseexercises;
+
+import com.javacourseexercises.ex001_list.Program;
+
+public class Main {
+    public static void main(String[] args) {
+        Program ex001List = new Program();
+        ex001List.program();
+    }
+}
