8f710b911fcdd9ece704d07c5f1885f6bf215341
Gabriel França <gabrielgfranca7@proton.me>
2026-04-08 20:22:20 -0300

    ex 001 : Listas

---
+++ src/main/java/com/javacourseexercises/ex001_list/Employee.java
@@ -0,0 +1,50 @@
+package com.javacourseexercises.ex001_list;
+
+public class Employee {
+    private String id;
+    private String name;
+    private Double salary;
+
+    public Employee() {
+	
+    }
+
+    public Employee(String id, String name, double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public void increaseSalary(double percentage) {        
+        this.salary += this.salary * percentage / 100;
+    }
+
+    @Override
+    public String toString() {
+		return id + ", " + name + ", " + String.format("%.2f", salary);
+	}
+}
