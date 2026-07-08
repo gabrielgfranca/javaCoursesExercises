cbde92ea2c3a533a73ec0bb034b7a9c6fb15ad6a
Gabriel França <gabrielgfranca7@proton.me>
2026-07-08 17:37:42 -0300

    feat: execptions - bad solution

--- src/main/java/com/javacourseexercises/exceptions1/Program.java
+++ src/main/java/com/javacourseexercises/exceptions1/Program.java
@@ -1,9 +1,5 @@
 package com.javacourseexercises.exceptions1;
 
-// Solution 1 (very bad): all the validation logic in the main program
-// Solution 2 (bad): method returning string
-// Solution 3 (good): exception handling
-
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -11,15 +7,10 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class Program {
-    public void run() {
+    public void run() throws ParseException {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-
-        sc.close();
-    }
-
-    private void solutionOne(Scanner sc, SimpleDateFormat sdf) throws ParseException {
         IO.println("Room Number: ");
         int number = sc.nextInt();
         sc.nextLine();
@@ -42,23 +33,13 @@ public class Program {
             IO.println("Check-out date (dd/MM/yyyy): ");
             checkOut = sdf.parse(sc.next());
 
-            Date now = new Date();
-            if (checkIn.before(now) || checkOut.before(now)) {
-                IO.println("Error in reservation: Reservation dates for update must be futures dates");
-            } else if (!checkOut.after(checkIn)) {
-                IO.println("Error in reservation: Check-out data must be after check-in date");
+            String errorMessage = reservation.updateDates(checkIn, checkOut);
+            if (errorMessage != null) {
+                IO.println(errorMessage);
             } else {
-                reservation.updateDates(checkIn, checkOut);
                 IO.println("Reservation: " + reservation);
             }
         }
-    }
-
-    private void solutionTwo(Scanner sc, SimpleDateFormat sdf) {
-
-    }
-
-    private void solutionThree(Scanner sc, SimpleDateFormat sdf) {
-
+        sc.close();
     }
 }
