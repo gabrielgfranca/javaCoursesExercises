d0557759c6193f5fdb5d162977b356d428c33c98
Gabriel França <gabrielgfranca7@proton.me>
2026-07-25 11:44:38 -0300

    feat: interface exercise 2

---
+++ src/main/java/com/javacourseexercises/Interface2/model/service/ContractService.java
@@ -0,0 +1,36 @@
+package com.javacourseexercises.Interface2.model.service;
+
+import com.javacourseexercises.Interface2.model.entities.Contract;
+import com.javacourseexercises.Interface2.model.entities.Installment;
+
+import java.time.LocalDate;
+
+public class ContractService {
+    private OnlinePaymentService onlinePaymentService;
+
+    public ContractService(OnlinePaymentService onlinePaymentService) {
+        this.onlinePaymentService = onlinePaymentService;
+    }
+
+    public OnlinePaymentService getOnlinePaymentService() {
+        return onlinePaymentService;
+    }
+
+    public void setOnlinePaymentService(OnlinePaymentService onlinePaymentService) {
+        this.onlinePaymentService = onlinePaymentService;
+    }
+
+    public void processContract(Contract contract, int months) {
+        double basicQuota = contract.getTotalValue() / months;
+
+        for (int i = 1; i <= months; i++) {
+            LocalDate dueDate = contract.getDate().plusMonths(i);
+
+            double interest = onlinePaymentService.interest(basicQuota, i);
+            double fee = onlinePaymentService.paymentFee(basicQuota + interest);
+            double quota = basicQuota + interest + fee;
+
+            contract.getInstallments().add(new Installment(dueDate, quota));
+        }
+    }
+}
--- src/main/java/com/javacourseexercises/Main.java
+++ src/main/java/com/javacourseexercises/Main.java
@@ -1,6 +1,6 @@
 package com.javacourseexercises;
 
-import com.javacourseexercises.Interface1.application.Program;
+import com.javacourseexercises.Interface2.application.Program;
 
 public class Main {
     public static void main(String[] args) {
