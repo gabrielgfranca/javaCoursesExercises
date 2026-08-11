e8a53cab7f12415a7e9358f53242129b8dadfa1f
Gabriel França <gabrielgfranca7@proton.me>
2026-08-11 18:31:52 -0300

    feat: set exercise 02

---
+++ src/main/java/com/javacourseexercises/set/exercise02/application/Program.java
@@ -0,0 +1,46 @@
+package com.javacourseexercises.set.exercise02.application;
+
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Set<Integer> a = new HashSet<>();
+        Set<Integer> b = new HashSet<>();
+        Set<Integer> c = new HashSet<>();
+
+        System.out.print("How many students for course A? ");
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            int student = sc.nextInt();
+            a.add(student);
+        }
+
+        System.out.print("How many students for course B? ");
+        n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            int student = sc.nextInt();
+            b.add(student);
+        }
+
+        System.out.print("How many students for course C? ");
+        n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            int student = sc.nextInt();
+            c.add(student);
+        }
+
+        Set<Integer> total = new HashSet<>(a);
+        total.addAll(b);
+        total.addAll(c);
+
+        System.out.println("Total number of students: " + total.size());
+
+        sc.close();
+    }
+}
