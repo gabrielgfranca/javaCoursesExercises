3a6c919d7c2e316d63b0446707b69497b8641437
Gabriel França <gabrielgfranca7@proton.me>
2026-08-15 23:41:08 -0300

    feat: Predicate lambda exercise

---
+++ src/main/java/com/javacourseexercises/lambda/predicate/ProductPredicate.java
@@ -0,0 +1,11 @@
+package com.javacourseexercises.lambda.predicate;
+
+import java.util.function.Predicate;
+
+public class ProductPredicate implements Predicate<Product> {
+
+    @Override
+    public boolean test(Product product) {
+        return product.getPrice() >= 100.0;
+    }
+}
---
+++ src/main/java/com/javacourseexercises/lambda/predicate/Program.java
@@ -0,0 +1,37 @@
+package com.javacourseexercises.lambda.predicate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("TV", 900.00));
+        list.add(new Product("Mouse", 50.00));
+        list.add(new Product("Tablet", 350.50));
+        list.add(new Product("HD Case", 80.90));
+
+        // Implementação da Interface
+        // list.removeIf(new ProductPredicate());
+
+        // Reference method com método estático
+        // list.removeIf(Product::staticProductPredicate);
+
+        // Reference method com método não estático
+        // list.removeIf(Product::nonStaticProductPredicate);
+
+        // Expressão lambda declarada
+        // Predicate<Product> predicate = p -> p.getPrice() < 100.00;
+        // list.removeIf(predicate);
+
+        // Expressão lambda inline
+        list.removeIf(p -> p.getPrice() < 100.00);
+
+
+        list.forEach(System.out::println);
+    }
+}
